f267c1e9914165a9de47b329fed6ad8f85f31c53
VyshakhC <85336601+VyshakhC@users.noreply.github.com>
2021-07-08 17:16:45 +0530

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -226,7 +226,7 @@ int delete(FILE *db_file, char *name) {
   entry *p = load_entries(db_file);
   entry *base = p;
   entry *prev = NULL;
-  entry del = NULL ; / Node to be deleted */
+  entry del = NULL ; /* Node to be deleted */
   int deleted = 0;
 
   while (p!=NULL) {
